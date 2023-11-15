c3bb395f60b8b01452511a351a20887a924fd603
faldor20 <eli.jambu@yahoo.com>
2023-11-16 05:15:53 +1000

    init

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+    #let
+      # defaultPackage = pkgs: pkgs.callPackage (nixpkgs + "/pkgs/development/tools/parsing/tree-sitter/grammar.nix") { } {
+      #   language = "norg";
+      #   source = ./.;
+      #   inherit (pkgs.tree-sitter) version;
+      # };
+    #in
+    (flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = import nixpkgs { inherit system; }; in
+        {
+          # defaultPackage = defaultPackage pkgs;
+          devShell = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [
+              nodejs
+              nodePackages.node-gyp
+              lldb
+              gdb
+              tree-sitter
+            ];
+          };
+        })); 
+
+        # // (let pkgs = import nixpkgs { }; in { defaultPackage = defaultPackage pkgs; });
+        
+}
---
+++ grammar.js
@@ -0,0 +1,8 @@
+module.exports = grammar({
+  name: 'YOUR_LANGUAGE_NAME',
+
+  rules: {
+    // TODO: add the actual grammar rules
+    source_file: $ => 'hello'
+  }
+});
