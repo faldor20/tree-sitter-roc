364cf00822ebcd00036974b8d2c52dc580e40f43
Alf Richter <h2000@users.noreply.github.com>
2024-02-04 12:53:37 +0100

    Update lib.rs lower case roc

--- bindings/rust/lib.rs
+++ bindings/rust/lib.rs
@@ -6,7 +6,7 @@
 //! ```
 //! let code = "";
 //! let mut parser = tree_sitter::Parser::new();
-//! parser.set_language(tree_sitter_ROC::language()).expect("Error loading ROC grammar");
+//! parser.set_language(tree_sitter_roc::language()).expect("Error loading ROC grammar");
 //! let tree = parser.parse(code, None).unwrap();
 //! ```
 //!
@@ -18,14 +18,14 @@
 use tree_sitter::Language;
 
 extern "C" {
-    fn tree_sitter_ROC() -> Language;
+    fn tree_sitter_roc() -> Language;
 }
 
 /// Get the tree-sitter [Language][] for this grammar.
 ///
 /// [Language]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Language.html
 pub fn language() -> Language {
-    unsafe { tree_sitter_ROC() }
+    unsafe { tree_sitter_roc() }
 }
 
 /// The content of the [`node-types.json`][] file for this grammar.
