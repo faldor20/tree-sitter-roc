795e7cef6af74207847b65fd52a157fab8c352b5
Jasper Woudenberg <mail@jasperwoudenberg.com>
2024-03-24 22:22:10 +0100

    Remove flake-compat dependency
    This doesn't appear to be used anymore!

--- flake.nix
+++ flake.nix
@@ -2,10 +2,6 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
   };
   outputs = { self, nixpkgs, flake-utils, ... }@inputs:
     #let
