ad5469d025e15e2067c1767b6051c1e09b564cca
nat-418 <93013864+nat-418@users.noreply.github.com>
2024-04-03 09:51:56 +0200

    feat(neovim): follow nvim-treesitter norms

--- neovim/queries/injections.scm
+++ neovim/queries/injections.scm
@@ -1,12 +1,5 @@
-;injection from function calls
-(function_call_expr
-  (variable_expr (identifier) @injection.language)
-  (const [(multiline_string)(string)] @injection.content)
-)
-;injection from piping function calls
-(bin_op_expr                                                                                      
-part: (const                                                                                  
-    [(multiline_string)(string)] @injection.content)
-part: (operator)                                                                                
-part: (variable_expr                                                                            
-  (identifier)@injection.language))   
+([
+  (line_comment)
+  (doc_comment)
+] @injection.content
+  (#set! injection.language "comment"))
