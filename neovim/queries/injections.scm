7df2c0892e62efb81a7504d9799d4e0d0443d241
faldor20 <eli.jambu@gmail.com>
2024-04-07 07:39:40 +1000

    Added injections for language inclusions

--- neovim/queries/injections.scm
+++ neovim/queries/injections.scm
@@ -1,3 +1,15 @@
+;injection from function calls
+(function_call_expr
+  (variable_expr (identifier) @injection.language)
+  (const [(multiline_string)(string)] @injection.content)
+)
+;injection from piping function calls
+(bin_op_expr                                                                                      
+  part: (const                                                                                  
+    [(multiline_string)(string)] @injection.content)
+  part: (operator)                                                                                
+  part: (variable_expr                                                                            
+    (identifier)@injection.language))   
 ([
   (line_comment)
   (doc_comment)
